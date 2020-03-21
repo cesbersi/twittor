ed8fbd51505d4bb8f3c2750e5ac26949ce12d4d3
Cesar Bernal <cesbersi@gmail.com>
2020-03-21 17:49:35 -0500

    version inicial

---
+++ js/sw-utils.js
@@ -0,0 +1,14 @@
+const actualizarCacheDinamico = ((dynamicCache, req, res) => {
+
+    if (res.ok) {
+        return caches.open(dynamicCache).then(
+            cache => {
+                cache.put(req, res.clone());
+                return res.clone();
+            }
+        );
+    } else {
+        return res;
+    }
+
+});
