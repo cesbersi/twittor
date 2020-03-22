36c5eaaab33d9c8326a7510df8054aea5b108a65
Cesar Bernal <cesbersi@gmail.com>
2020-03-22 14:19:42 -0500

    version 0.1.3

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v3';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE = 'static-v4';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
@@ -61,6 +61,10 @@ self.addEventListener('activate', e => {
         keys.forEach(key => {
 
             if (key !== STATIC_CACHE && key.includes('static')) {
+                return caches.delete(key);
+            }
+
+            if (key !== DYNAMIC_CACHE && key.includes('dynamic')) {
                 return caches.delete(key);
             }
 
