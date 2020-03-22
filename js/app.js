6408d821abf7ea4977b4a26f728e65f34eed785a
Cesar Bernal <cesbersi@gmail.com>
2020-03-21 19:56:13 -0500

    version 0.1

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 
+var url = window.location.href;
+var swLocation = '/twittor/sw.js';
+
 
 if ( navigator.serviceWorker ) {
-    navigator.serviceWorker.register('/sw.js');
+
+    if ( url.includes('localhost')){
+        swLocation  = '/sw.js';
+    }
+
+    navigator.serviceWorker.register(swLocation);
 }
 
 // Referencias de jQuery
--- sw.js
+++ sw.js
@@ -5,16 +5,16 @@ const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
-    '/',
-    '/index.html',
-    '/css/style.css',
-    '/img/favicon.ico',
-    '/img/avatars/hulk.jpg',
-    '/img/avatars/ironman.jpg',
-    '/img/avatars/spiderman.jpg',
-    '/img/avatars/thor.jpg',
-    '/img/avatars/wolverine.jpg',
-    '/js/app.js',
+    //'/',
+    'index.html',
+    'css/style.css',
+    'img/favicon.ico',
+    'img/avatars/hulk.jpg',
+    'img/avatars/ironman.jpg',
+    'img/avatars/spiderman.jpg',
+    'img/avatars/thor.jpg',
+    'img/avatars/wolverine.jpg',
+    'js/app.js',
     'js/sw-utils.js' 
 ];
 
